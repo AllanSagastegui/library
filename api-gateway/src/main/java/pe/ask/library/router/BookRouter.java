1658b1aee003fb5876ec34f11af6532c2cb9209e
AllanSagastegui <xpheri035@gmail.com>
2025-11-21 21:55:55 -0500

    feat: update config modules

---
+++ api-gateway/src/main/java/pe/ask/library/router/BookRouter.java
@@ -0,0 +1,21 @@
+package pe.ask.library.router;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import pe.ask.library.config.LoadBalancerConfig;
+
+@Configuration
+@LoadBalancerClient(name = "book-service", configuration = LoadBalancerConfig.class)
+public class BookRouter {
+
+    @Bean
+    public RouteLocator bookRoutes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("book-service", r -> r.path("/api/v1/books/**")
+                        .uri("lb://book-service"))
+                .build();
+    }
+}
